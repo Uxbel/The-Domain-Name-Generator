c2d3ea50d02c18d23c5d912d88e4945c78ffe59a
Uxbel <beldetes@gmail.com>
2021-08-05 14:43:40 +0000

    Carga del ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-domain").innerHTML = generateDomain();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "best", "premium", "vip", "top"];
+  let noun = ["computer", "sofa", "carpet", "bottle", "bed"];
+  let extension = [".com", ".net", ".org", ".info"];
+  let sumParts = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          sumParts +=
+            pronoun[i] + adj[j] + noun[k] + extension[l] + "<br></br>";
+        }
+      }
+    }
+  }
+  return sumParts;
 };
